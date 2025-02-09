b210aa3cb6809b6edb2450870ed9de3bd6aa8009
jay-raut <jayraut@my.yorku.ca>
2025-02-09 00:11:57 -0500

    added auth route template

--- middleware/api-gateway/server.js
+++ middleware/api-gateway/server.js
@@ -6,6 +6,7 @@ const express = require("express"); //libraries
 const auction_route = require("./routes/auction_routes");
 const notification_route = require("./routes/notification_routes");
 const payment_route = require("./routes/payment_routes");
+const authentication_routes = require("./routes/authentication_routes")
 const app =  express();
 
 
@@ -14,5 +15,6 @@ const app =  express();
 app.use("/api/auction", auction_route); //routes
 app.use("/api/notification", notification_route);
 app.use("/api/payment", payment_route);
+app.use("/api/authentication", authentication_routes);
 
 app.listen(server_port, console.log(`API server started on port ${server_port}`));
