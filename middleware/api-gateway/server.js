f753aa70f46a0fe8925a8b22f02f02e1e1922cd2
jay-raut <jayraut@my.yorku.ca>
2025-02-09 00:04:37 -0500

    Foundation for api gateway
    Using npm to install packages and implemented template for api gateway

---
+++ middleware/api-gateway/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config(); //environment variables 
+const server_port = process.env.api_server_port;
+
+
+const express = require("express"); //libraries
+const auction_route = require("./routes/auction_routes");
+const notification_route = require("./routes/notification_routes");
+const payment_route = require("./routes/payment_routes");
+const app =  express();
+
+
+
+
+app.use("/api/auction", auction_route); //routes
+app.use("/api/notification", notification_route);
+app.use("/api/payment", payment_route);
+
+app.listen(server_port, console.log(`API server started on port ${server_port}`));
