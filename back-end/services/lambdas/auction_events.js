21d642c374fe346f1942804a2606ca946f440cb8
jay-raut <jayraut@my.yorku.ca>
2025-03-02 01:10:18 -0500

    Checking if auction needs to end implemented.

--- back-end/services/lambdas/auction_events.js
+++ back-end/services/lambdas/auction_events.js
@@ -8,16 +8,34 @@ redis_client.connect();
 redis_client.on("connect", () => console.log("Redis connected"));
 redis_client.on("error", (err) => console.error("Redis connection error:", err));
 
+const check_ending = {
+  forward_auction: check_forward_ending,
+};
+
+
+var now;
 async function check_auctions() {
-  const now = Date.now();
+  now = Date.now();
   const get_auctions = await redis_client.keys("auction:*");
   get_auctions.forEach(async (auction_key) => {
     const auction_value = await redis_client.hGetAll(auction_key);
     if (new Date(auction_value.start_time).getTime() < now && auction_value.is_active != true) {
-      console.log(auction_key);
+      console.log(`${auction_key} needs to start`);
+    }
+    if (check_ending[auction_value.auction_type]) {
+      const needs_ending = await check_ending[auction_value.auction_type](auction_value);
+      if (needs_ending) {
+        console.log(`${auction_key} needs to end`);
+      }
     }
   });
   console.log();
+}
+async function check_forward_ending(auction_value) {
+  if (new Date(auction_value.end_time).getTime() < now && auction_value.is_active == true) {
+    return true;
+  }
+  return false;
 }
 
 setInterval(check_auctions, 5_000); //job runs every 5 seconds
