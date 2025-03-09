7fb6bc6408fe4b1a73f3c1108d4cc5869db186e7
jay-raut <jayraut@my.yorku.ca>
2025-03-09 13:50:46 -0400

    added job worker to backend

--- back-end/services/start.sh
+++ back-end/services/start.sh
@@ -4,5 +4,6 @@ env-cmd -f ./auction/docker.env node ./auction/server.js &
 env-cmd -f ./orders/docker.env node ./orders/server.js &
 env-cmd -f ./notification/docker.env node ./notification/server.js &
 env-cmd -f ./authentication/docker.env node ./authentication/server.js &
+env-cmd -f ./jobs/.env node ./jobs/auction_events.js &
 
 wait
