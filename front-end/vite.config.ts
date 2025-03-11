89a3b8201274cad4e29a47831ebf592d2c7b1057
hayyaanp <95776319+hayyaanp@users.noreply.github.com>
2025-03-11 10:03:22 -0400

    configure vite to always start on specific port

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 5173,
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
